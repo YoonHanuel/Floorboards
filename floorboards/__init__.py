9898c6f33186c7246c7304dd1e4d5db7a27768a5
YoonHanuel <hanhan.hanuel@gmail.com>
2026-09-06 20:18:53 -0400

    Add Floorboards package

---
+++ floorboards/__init__.py
@@ -0,0 +1,6 @@
+"""Floorboards: tiny falsifiable software assumptions."""
+
+from .core import Specimen, load_specimens, render_specimen
+
+__all__ = ["Specimen", "load_specimens", "render_specimen"]
+__version__ = "0.1.0"
